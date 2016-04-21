e1a662a43654e26fc3d3bf453c811b5a27834510
Raphael Brunner <raphael@raphaelbrunner.ch>
2016-04-21 14:50:55 +0200

    initial plotter commit

---
+++ 3D/InfoVisClustering_sample/Assets/MakeViz.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeViz : MonoBehaviour {
+    public float instanceScale = 1.0f;
+
+    public int dataColumnX = 0;
+    public int dataColumnY = 1;
+    public int dataColumnZ = 2;
+    public int dataColumnColour = 3;
+
+    public float posScaleX = 10.0f;
+    public float posScaleY = 10.0f;
+    public float posScaleZ = 10.0f;
+
+    // These cache the corresponding public dataColumn fields
+    // so that we can detect changes made in Editor in Update()
+    int m_dcX, m_dcY, m_dcZ, m_dcColour;
+
+    ArrayList m_objectArray;
+    ParseDB m_dataSource;
+
+	// Use this for initialization
+	void Start () {
+        m_dataSource = GetComponent<ParseDB>();
+        if (m_dataSource != null)
+        {
+            make3DCloud();
+            update3DCloud(dataColumnX, dataColumnY, dataColumnZ,dataColumnColour);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Update coloumn assignment if one or more fields have changed
+	    if ( (dataColumnX != m_dcX) || (dataColumnY != m_dcY) || (dataColumnZ != m_dcZ) || (dataColumnColour != m_dcColour) )
+            update3DCloud(dataColumnX, dataColumnY, dataColumnZ, dataColumnColour);
+    }
+
+    void update3DCloud(int colX, int colY, int colZ,int colColour)
+    {
+        m_dcX = colX;
+        m_dcY = colY;
+        m_dcZ = colZ;
+        m_dcColour = colColour;
+
+        float minX, maxX, minY, maxY, minZ, maxZ, minColour, maxColour, scaleX, scaleY, scaleZ, scaleColour;
+        minX = maxX = minY = maxY = minZ = maxZ = minColour = maxColour = 0.0f;
+        scaleX = scaleY = scaleZ = scaleColour = 1.0f;
+
+        if (m_dataSource.GetDataValid())
+        {
+            m_dataSource.GetColumnMinMaxValues(ref minX, ref maxX, colX);
+            m_dataSource.GetColumnMinMaxValues(ref minY, ref maxY, colY);
+            m_dataSource.GetColumnMinMaxValues(ref minZ, ref maxZ, colZ);
+            m_dataSource.GetColumnMinMaxValues(ref minColour, ref maxColour, colColour);
+
+            scaleX = 1.0f / (maxX - minX);
+            scaleY = 1.0f / (maxY - minY);
+            scaleZ = 1.0f / (maxZ - minZ);
+            scaleColour = 1.0f / (maxColour - minColour);
+
+            // Update position and colour of the 3D cloud objects
+            for (int i = 0; i < m_dataSource.numRows; i++)
+            {
+                float newPosX = 0.0f;
+                float newPosY = 0.0f;
+                float newPosZ = 0.0f;
+                float newColour = 0.0f;
+                m_dataSource.GetArrayValue(ref newPosX, i, colX);
+                newPosX = (newPosX - minX) * scaleX * posScaleX;
+                m_dataSource.GetArrayValue(ref newPosY, i, colY);
+                newPosY = (newPosY - minY) * scaleY * posScaleY;
+                m_dataSource.GetArrayValue(ref newPosZ, i, colZ);
+                newPosZ = (newPosZ - minZ) * scaleZ * posScaleZ;
+                m_dataSource.GetArrayValue(ref newColour, i, colColour);
+                newColour = (newColour - minColour) * scaleColour;
+
+                // Compute RGB colour by interpolating between red and green
+                Color newRGB;
+                newRGB.r = 1.0f - newColour;
+                newRGB.g = newColour;
+                newRGB.b = 0.0f;
+                newRGB.a = 1.0f;
+
+                GameObject obj = (GameObject)m_objectArray[i];
+
+                obj.transform.position = new Vector3(newPosX, newPosY, newPosZ);
+                obj.transform.localScale = new Vector3(instanceScale, instanceScale, instanceScale);
+
+                // Set colour of object
+                MeshRenderer rdr = obj.GetComponent<MeshRenderer>();
+                if (rdr != null)
+                    rdr.material.color = newRGB;
+            }
+        }
+    }
+
+    // construct a point cloud from database entries
+    void make3DCloud ()
+    {
+        int rowCount = m_dataSource.GetRowCount();
+        m_objectArray = new ArrayList();
+
+        for (int i=0;i<rowCount;i++)
+        {
+            GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            string objectName = string.Format("Instance_{0}", i);
+            newObject.name = objectName;
+            newObject.transform.parent = gameObject.transform;
+            m_objectArray.Add(newObject);
+        }
+    }
+
+}
---
+++ 3D/InfoVisClustering_sample/Assets/ParseDB.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ParseDB : MonoBehaviour {
+    public string filePath;
+    public int numRows;
+    public int numColumns;
+
+    float[,] m_dataArray;
+    bool m_dataValid = false;
+
+	// Use this for initialization
+	void Start () {
+        if (filePath.Length > 0)
+        {
+            if (ReadFileToArray(filePath))
+            {
+                numRows = m_dataArray.GetLength(0);
+                numColumns = m_dataArray.GetLength(1);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool GetDataValid()
+    {
+        return m_dataValid;
+    }
+
+    public int GetRowCount()
+    {
+        return m_dataArray.GetLength(0);
+    }
+
+    public int GetColumnCount()
+    {
+        return m_dataArray.GetLength(1);
+    }
+
+    public bool GetColumnMinMaxValues(ref float minVal, ref float maxVal, int col)
+    {
+        if (m_dataValid && (col >= 0) && (col < numRows) )
+        {
+            minVal = float.MaxValue;
+            maxVal = float.MinValue;
+
+            for (int i=0; i<numRows; i++)
+            {
+                if (m_dataArray[i, col] < minVal)
+                    minVal = m_dataArray[i, col];
+
+                if (m_dataArray[i, col] > maxVal)
+                    maxVal = m_dataArray[i, col];
+            }
+        }
+        return m_dataValid;
+    }
+
+    public bool GetArrayValue(ref float value, int row, int col)
+    {
+        if (m_dataValid)
+            value = m_dataArray[row, col];
+
+        return m_dataValid;
+    }
+
+    bool ReadFileToArray(string fp)
+    {
+        int lineCount = 0;
+        string line;
+
+        m_dataValid = false;
+
+        // construct StreamReader for the file given
+        System.IO.StreamReader file = new System.IO.StreamReader(fp);
+
+        // first pass: determine number of lines in source file
+        while (file.ReadLine() != null)
+            lineCount++;
+
+        int rowCount = lineCount;
+        int columnCount = 0;
+        lineCount = 0;
+
+        // reset file stream to beginning for actual reading of data
+        file.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+
+        while ((line = file.ReadLine()) != null)
+        {
+            //string[] items = line.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
+			string[] items = line.Split (',');
+			//for (int i = 0; i < items.Length; i++) {
+				//if (items [i] == null || items [i].Equals ("NULL")) {
+					//items [i] = "0";
+				//}
+			//}
+
+            if (items.Length > 0)
+            {
+                if (lineCount == 0)
+                {
+                    columnCount = items.Length;
+                    m_dataArray = new float[rowCount, columnCount];
+                }
+
+                for (int i=0;i<columnCount;i++)
+                {
+                    m_dataArray[lineCount, i] = int.Parse(items[i], System.Globalization.NumberStyles.Any);
+                }
+
+                lineCount++;
+            }
+        }
+
+        if ((columnCount > 0) && (rowCount > 0))
+            m_dataValid = true;
+
+        file.Close();
+
+        return m_dataValid;
+    }
+
+    
+
+}
